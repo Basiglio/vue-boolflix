b23bdfed6ec7d5b650fcb30a54285637cf210845
Emilo <emilio.furnari87@gmail.com>
2020-12-18 16:54:07 +0100

    collegata richiesta

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message:"",
+    films: [],
+  },
+  methods: {
+    search: function () {
+      axios.get("https://api.themoviedb.org/3/search/movie?api_key=cfdd37ec50ecc36f0abe0f17a31c2b48" + "&query=" + this.message)
+      .then((response) => {
+        this.films = response.data.results;
+        console.log(this.films);
+      });
+    }
+  }
+})
